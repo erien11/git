c6fd340f13a4b4b3cdb3723264698f2c181fbe23
milosssss9 <milosz.borczynski12@gmail.com>
2022-11-03 12:55:01 +0100

    policz2

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int Policz(int a, int b){
 }
 int main() {
     printf("Hello, World!\n");
-    printf("W pierwszym branchu 2+3 = %d", Policz(2,3));
+    printf("W drugim branchu 2+3 = %d", Policz(2,3));
     return 0;
 }
